0d4de9c286b4c205c77f9f230b27ae5b52d81280
Quang Ly <quangdly@gmail.com>
2013-07-11 02:23:33 +0000

    read from index

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('file.html'),'utf-8');
+
+/*
+var buffer = fs.readFile('index.html', function (err, data){
+    if (err) throw err;
+    //console.log(data);
+    return data;
+});
+*/
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
